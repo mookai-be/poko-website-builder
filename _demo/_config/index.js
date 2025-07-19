447e83a0a13f03a5af229a6036d0d31b383f0fb0
m4rrc0 <github-h9s2@m4rr.co>
2025-07-19 21:50:24 +0200

    setup _content folder and _demo content

---
+++ _demo/_config/index.js
@@ -0,0 +1,16 @@
+// import {
+//   NODE_ENV,
+//   WORKING_DIR_ABSOLUTE,
+//   CONTENT_PATH_PREFIX,
+//   CONTENT_DIR,
+//   PROD_URL,
+//   DISPLAY_URL,
+//   CMS_AUTH_URL,
+//   CMS_REPO,
+//   CMS_BACKEND,
+//   CMS_BRANCH,
+// } from "../../env.config.js";
+
+export const collections = [];
+
+export const singletons = [];
