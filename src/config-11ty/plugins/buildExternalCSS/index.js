97e5cc3def9e75d462740b9ded00c0ef71a672ff
m4rrc0 <github-h9s2@m4rr.co>
2025-09-26 17:17:46 +0200

    fix: buildExternalCSS when no stylesheet is provided

--- src/config-11ty/plugins/buildExternalCSS/index.js
+++ src/config-11ty/plugins/buildExternalCSS/index.js
@@ -14,17 +14,23 @@ export default async function (eleventyConfig, pluginOptions) {
     (entrypoint) => !entrypoint.split("/").pop().startsWith("_")
   );
 
-  await bunBuild({
-    entrypoints,
-    // outdir: "./dist/assets/styles",
-    outdir: `${dir.output}/${outputDir}`,
-    // naming: '[name].css',
-    // naming: "index.css",
-    // plugins: [cssTransformPlugin],
-    minify: MINIFY,
-    cssChunking: true,
-  }).catch((e) => {
-    console.error(e);
-    throw e;
-  });
+  if (Array.isArray(entrypoints) && typeof entrypoints[0] === "string") {
+    await bunBuild({
+      entrypoints,
+      // outdir: "./dist/assets/styles",
+      outdir: `${dir.output}/${outputDir}`,
+      // naming: '[name].css',
+      // naming: "index.css",
+      // plugins: [cssTransformPlugin],
+      minify: MINIFY,
+      cssChunking: true,
+    }).catch((e) => {
+      console.error(e);
+      throw e;
+    });
+  }
+
+  // TODO: add global data for generated stylesheets urls ? (Should we account for drafts? Probably not as we can use underscores in file names?)
+  // TODO: integrate global variables in the first stylesheet generated
+  // TODO: if no stylesheet is generated, add a global data variable to be added to the main layout for global css variables
 }
